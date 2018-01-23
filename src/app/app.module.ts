d95f3e62dbc2a30af13004d223deda7f4645b18d
Diana Sutandie <sutandie@ca.ibm.com>
2018-01-23 00:41:19 -0500

    initial commit for website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutUsPopupComponent } from './about-us-popup/about-us-popup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsPopupComponent
   ],
   imports: [
     BrowserModule
